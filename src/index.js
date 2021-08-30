e6c2cb62228d60e4a34d3644c549bd251f974105
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-30 14:46:31 -0600

    Create Routes and build navBar.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import bootstrap from 'bootstrap';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
