cdb42ac712364270020d42da65bb75a9a9c4fe94
afcastaneda223 <af.castaneda223@gmail.com>
2021-08-31 22:00:53 -0300

    add reserved badge, and button functionality

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getRocket, bookRocket } from '../redux/rockets/rockets';
+import { getRocket, bookRocket, unbookRocket } from '../redux/rockets/rockets';
 
 function RocketList() {
   const dispatch = useDispatch();
@@ -23,10 +23,14 @@ function RocketList() {
     dispatch(bookRocket(e.target.id));
   };
 
+  const cancelReserve = (e) => {
+    dispatch(unbookRocket(e.target.id));
+  };
+
   return (
     <div>
       {myRocketArray.map((rocket) => (
-        <div className="card mb-3" key={rocket.id}>
+        <div className="card mb-5 border-0 " key={rocket.id}>
           <div className="row g-0">
             <div className="col-lg-4">
               <img src={rocket.flickr_images} className="img-fluid rounded-start" alt={rocket.rocket_name} />
@@ -34,8 +38,11 @@ function RocketList() {
             <div className="col-lg-8">
               <div className="card-body">
                 <h5 className="card-title">{rocket.rocket_name}</h5>
-                <p className="card-text">{rocket.description}</p>
-                <button type="button" onClick={reserveRocket} id={rocket.id} className="btn btn-primary mt-lg-5">Reserve Rocket</button>
+                <p className="card-text">
+                  { rocket.reserved ? <span className="badge me-2">Reserved</span> : null}
+                  {rocket.description}
+                </p>
+                { rocket.reserved ? <button type="button" onClick={cancelReserve} id={rocket.id} className="btn btn-outline-primary mt-lg-5">Cancel Reservation</button> : <button type="button" onClick={reserveRocket} id={rocket.id} className="btn btn-primary mt-lg-5">Reserve Rocket</button>}
               </div>
             </div>
           </div>
