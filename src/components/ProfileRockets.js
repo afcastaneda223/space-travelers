6c5cb4e75c868b6c0430b4f9b7058ce3640f6706
afcastaneda223 <af.castaneda223@gmail.com>
2021-09-01 12:42:17 -0300

    add list of reserved rockets to myprofile

---
+++ src/components/ProfileRockets.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+function ProfileRockets() {
+  const myRocketArray = useSelector((state) => state.rockets);
+
+  return (
+    <div>
+      <ul className="list-group">
+        {myRocketArray.map((rocket) => (
+          <div key={rocket.id}>
+            { rocket.reserved ? <li className="list-group-item list-group-item-action py-3">{rocket.rocket_name}</li> : null}
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProfileRockets;
